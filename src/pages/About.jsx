38c6e4c605cfea1efd37715a091b71806dad6bc8
Aspiring2 <66567152+Aspiring2@users.noreply.github.com>
2023-07-10 11:08:12 +0300

    About add info

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -30,30 +30,41 @@ const About = () => {
       <Avatar className={classes.avatar} src="https://rusanov-d.ru/api/uploads/IMG_20221204_140644.jpg" alt="Avatar" />
       <div className={classes.content}>
         <h2>About Me</h2>
-        <ReactMarkdown>Skills and Experience:
-- Strong skills and experience in JavaScript frontend and backend development 😊
-- Proficiency with development tools for JavaScript, both frontend and backend 🛠️
-- Web design and development experience using ReactJS frontend and NodeJS frameworks and libraries 💻
-- Understanding and working knowledge of Restful APIs, with experience working with them 🌐
-- Familiarity with React-Redux is an advantage 🚀
+        <ReactMarkdown>
+        <h2>Skills and Experience:</h2>
+<ul>
+  <li>Strong skills and experience in JavaScript frontend and backend development 😊</li>
+  <li>Proficiency with development tools for JavaScript, both frontend and backend 🛠️</li>
+  <li>Web design and development experience using ReactJS frontend and NodeJS frameworks and libraries 💻</li>
+  <li>Understanding and working knowledge of Restful APIs, with experience working with them 🌐</li>
+  <li>Familiarity with React-Redux is an advantage 🚀</li>
+</ul>
 
-Database Experience:
-- Skills and experience in using relational database models 🗃️
-- Proficiency in using database management tools and languages, such as MySQL, PostgreSQL, Express, and MongoDB 📊
+<h2>Database Experience:</h2>
+<ul>
+  <li>Skills and experience in using relational database models 🗃️</li>
+  <li>Proficiency in using database management tools and languages, such as MySQL, PostgreSQL, Express, and MongoDB 📊</li>
+</ul>
 
-UX/UI Design:
-- A working understanding of UX/UI design and development 🎨
+<h2>UX/UI Design:</h2>
+<ul>
+  <li>A working understanding of UX/UI design and development 🎨</li>
+</ul>
 
-Product Development:
-- Experience in developing and creating products using ReactJS and MongoDB 🚀
-- Over 3 years of IT experience which includes half a year of extensive experience as a React JS Developer ⚛️
-- Experienced in MEAN stack development (MongoDB, Express, Angular, Node.js) 📦
-- Extensive knowledge in developing single-page applications (SPAs) 🌐
-- Good experience in customizing CSS frameworks like MUI, ANTD, and Foundation using CSS pre-processors LESS or SASS 💅
-- Good expertise in analyzing the Document Object Model (DOM) Layout, DOM Functions, and JavaScript functions, Cascading Styles across cross-browser using Firebug, Developer Tool Bar 🔍
-- Expertise in React JS framework to develop SPAs and working with React Flux architecture and Redux ⚛️🔁
-- Ability to work effectively while working as a team member as well as individually 🤝
-- Excellent communication and interpersonal skills, well-organized, goal-oriented ✉️🤝</ReactMarkdown>
+<h2>Product Development:</h2>
+<ul>
+  <li>Experience in developing and creating products using ReactJS and MongoDB 🚀</li>
+  <li>Over 3 years of IT experience which includes half a year of extensive experience as a React JS Developer ⚛️</li>
+  <li>Experienced in MEAN stack development (MongoDB, Express, Angular, Node.js) 📦</li>
+  <li>Extensive knowledge in developing single-page applications (SPAs) 🌐</li>
+  <li>Good experience in customizing CSS frameworks like MUI, ANTD, and Foundation using CSS pre-processors LESS or SASS 💅</li>
+  <li>Good expertise in analyzing the Document Object Model (DOM) Layout, DOM Functions, and JavaScript functions, Cascading Styles across cross-browser using Firebug, Developer Tool Bar 🔍</li>
+  <li>Expertise in React JS framework to develop SPAs and working with React Flux architecture and Redux ⚛️🔁</li>
+  <li>Ability to work effectively while working as a team member as well as individually 🤝</li>
+  <li>Excellent communication and interpersonal skills, well-organized, goal-oriented ✉️🤝</li>
+</ul>
+
+        </ReactMarkdown>
       </div>
     </div>
   );
